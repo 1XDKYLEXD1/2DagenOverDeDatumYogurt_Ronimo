0903a3d0421c6779d39cd3eb689836134ab2f188
1XDKYLEXD1 <520163@student.glu.nl>
2019-03-24 21:00:14 +0100

    Unit editor gemaakt

--- Ronimo_2DagenOverDeDatumYogurt/Assets/Scripts/Kyle/UnitCreator.cs
+++ Ronimo_2DagenOverDeDatumYogurt/Assets/Scripts/Kyle/UnitCreator.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AUnit", menuName = "Units/Create Unit")]
 public class UnitCreator : ScriptableObject
 {
+    public enum UnitType
+    {
+        Spellcaster,
+        Miner,
+        Archer,
+        Melee,
+        Tank
+    };
+
     public string m_unitname;
     public int m_worthkilling;
     public int m_health;
     public float m_movementspeed;
     public float m_damageamount;
     public int m_buildcost;
+    public UnitType m_unittype;
 }
