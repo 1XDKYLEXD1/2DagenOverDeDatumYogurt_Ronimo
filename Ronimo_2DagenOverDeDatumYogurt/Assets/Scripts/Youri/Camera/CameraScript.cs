d156f44de5425144a3fa79b65c2938afdd3103a2
Dryomc <dryomc123@gmail.com>
2019-04-08 11:07:40 +0200

    Camera + Basic Enemy AI

---
+++ Ronimo_2DagenOverDeDatumYogurt/Assets/Scripts/Youri/Camera/CameraScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour
+{
+    public float _MinXPosition, _MaxXPosition, _Speed;
+
+    private void Update()
+    {
+        MoveWithKeyboard();
+        MoveWithMouse();
+    }
+
+    private float CalculateRelativePercentage(float one, float two)
+    {
+        float Percentage;
+
+        Percentage = ((one / two) * 100);
+
+        return Percentage;
+    }
+
+    #region Movement
+
+    private void MoveWithMouse()
+    {
+        Vector2 mousePos;
+        mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        float screenWidth = Screen.width;
+
+        float zero, twenty, eighty, hundred;
+
+        zero = 0;
+        twenty = screenWidth * 0.2f;
+        eighty = screenWidth * 0.8f;
+        hundred = screenWidth;
+
+        //Debug.Log("Twenty Percent: " + twenty);
+        //Debug.Log("Eighty Percent: " + eighty);
+        //Debug.Log("Onehundred Percent: " + hundred);
+
+        //Debug.Log("Mouse Position: " + mousePos.x);
+
+        if(transform.position.x > _MinXPosition || transform.position.x < _MaxXPosition)
+        {
+            if (mousePos.x < twenty && mousePos.x > zero)
+            {
+                transform.position -= -Vector3.left * Time.deltaTime * _Speed;
+            }
+
+            if (mousePos.x > eighty && mousePos.x < hundred)
+            {
+                transform.position -= -Vector3.right * Time.deltaTime * _Speed;
+            }
+        }      
+    }
+
+    private void MoveWithKeyboard()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > _MinXPosition)
+        {
+            transform.position -= -Vector3.left * Time.deltaTime * _Speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < _MaxXPosition)
+        {
+            transform.position -= -Vector3.right * Time.deltaTime * _Speed;
+        }
+
+        if (transform.position.x < _MinXPosition)
+        {
+            transform.position = new Vector3(_MinXPosition, 0, 0);
+        }
+        if (transform.position.x > _MaxXPosition)
+        {
+            transform.position = new Vector3(_MaxXPosition, 0, 0);
+        }
+    }
+
+    #endregion
+
+}
---
+++ Ronimo_2DagenOverDeDatumYogurt/Assets/Scripts/Youri/Enemy/EnemyAI.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _Units;
+
+    [SerializeField]
+    private GameObject _SpawnPoint;
+
+    private float _Timer;
+    private int _Index;
+    private int _SpawnCount;
+
+    [SerializeField]
+    private float _TimeUntillSpawn;
+
+    void Start()
+    {
+        _SpawnCount = 0;
+        _Timer = 0;
+        _Index = 0;
+    }
+
+    void Update()
+    {
+        _Timer += Time.deltaTime;
+
+        if(_Timer >= _TimeUntillSpawn)
+        {
+            SpawnUnit();
+            _Timer = 0;
+        }
+    }
+
+    private void SpawnUnit()
+    {
+        UnitDataGetter udg = _Units[_Index].GetComponent<UnitDataGetter>();
+        UnitCreator.UnitType unitType = udg.UnitData.m_unittype;
+        
+        if(udg != null)
+        {
+            switch (unitType)
+            {
+                case UnitCreator.UnitType.Melee:
+                    if(_SpawnCount < 3)
+                    {
+                        Instantiate(_Units[_Index], _SpawnPoint.transform.position, Quaternion.identity);
+                        _SpawnCount += 1;
+                    }                   
+                    else if(_SpawnCount >= 3)
+                    {
+                        _Index = 2;
+                        _SpawnCount = 0;
+                    }
+                    break;
+                case UnitCreator.UnitType.Archer:
+                    Instantiate(_Units[_Index], _SpawnPoint.transform.position, Quaternion.identity);
+                    _Index = 3;
+                    break;
+                case UnitCreator.UnitType.Miner:
+                    Instantiate(_Units[_Index], _SpawnPoint.transform.position, Quaternion.identity);
+                    _Index = 0;
+                    break;
+                case UnitCreator.UnitType.Spellcaster:
+                    Instantiate(_Units[_Index], _SpawnPoint.transform.position, Quaternion.identity);
+                    _Index = 0;
+                    break;
+                case UnitCreator.UnitType.Tank:
+                    Instantiate(_Units[_Index], _SpawnPoint.transform.position, Quaternion.identity);
+                    _Index = 1;
+                    break;
+            }
+        }
+        else if(udg == null)
+        {
+            Debug.Log("Unit Creator Script Is Null");
+        }
+        
+    }
+}
