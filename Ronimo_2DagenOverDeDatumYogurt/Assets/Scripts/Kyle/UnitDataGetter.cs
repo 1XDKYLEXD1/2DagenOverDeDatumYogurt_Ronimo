e2292413d209b7cdb32f50736c8ad72ff78c13cc
1XDKYLEXD1 <520163@student.glu.nl>
2019-04-14 18:17:59 +0200

    Building a Turret

--- Ronimo_2DagenOverDeDatumYogurt/Assets/Scripts/Kyle/UnitDataGetter.cs
+++ Ronimo_2DagenOverDeDatumYogurt/Assets/Scripts/Kyle/UnitDataGetter.cs
@@ -6,11 +6,24 @@ public class UnitDataGetter : MonoBehaviour
 {
     [SerializeField] UnitCreator m_unitdata;
 
+    int m_currenthealth;
+
+    void Start()
+    {
+        m_currenthealth = m_unitdata.m_health;    
+    }
+
     public UnitCreator UnitData
     {
         get
         {
             return m_unitdata;
         }
+    }
+
+    public int CurrentHealth
+    {
+        get { return m_currenthealth; }
+        set { m_currenthealth = (int)value; }
     }
 }
